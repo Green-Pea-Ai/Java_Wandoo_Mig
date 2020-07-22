a3a87aba9114f55f9cd59ecf0a959ddc34651e20
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-07-22 11:31:57 +0900

    200722 Wed 11:31

---
+++ src/Sixth_200722/HashMapProb.java
@@ -0,0 +1,39 @@
+package Sixth_200722;
+
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class HashMapProb {
+    public static void main(String[] args) {
+        final int START = 0;
+        final int MAX = 200;
+        final int RANGE = 51;
+        final int BIAS = 10;
+
+        // 값 초기화
+        Random rand = new Random();
+        Map<Integer, Integer> m =
+                new HashMap<Integer, Integer>();
+
+        Integer[] samples = new Integer[MAX];
+
+        for(int i = START; i < MAX; i++) {
+            samples[i] = rand.nextInt(RANGE) + BIAS;
+        }
+
+        // 실질적 코드
+        for(Integer sample: samples) {
+            Integer freq = m.get(sample);
+            m.put(sample, (freq == null) ? 1 : freq + 1);
+        }
+
+        // 출력
+        // 결과 : 해쉬맵이 짱이다.
+        System.out.println(m.size() + "개의 샘플이 있다.");
+        System.out.println(m.containsKey(17));
+        System.out.println(m.isEmpty());
+        System.out.println(m);
+    }
+}
