29601b6bd7a0943429667059183ef506ebc0c794
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-08-24 23:56:26 +0900

    200824 Mon 11:55 PT, 개인 프로젝트 설정 문제로 삽질함, 소스 코드 분석에도 시간 소요.
    for문 별찍기로 오늘의 커밋 대체.

---
+++ src/Personal_Training/PT_ForExam.java
@@ -0,0 +1,24 @@
+package Personal_Training;
+
+public class PT_ForExam {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                System.out.print("★");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("=======================");
+
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("★");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("=======================");
+    }
+}
