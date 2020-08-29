b0bc9b2ee43990d5c608ea51edf549f8862f3bb7
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-08-29 23:58:43 +0900

    200829 Sat 11:57 PT, 비대면 멘토링 진행. MVC 패턴 관련 수업.
    코드 올리려고 했으나 문제 발생. 곧 올릴 예정.

---
+++ src/Personal_Training/PT_200829_Sat.java
@@ -0,0 +1,5 @@
+package Personal_Training;
+
+public class PT_200829_Sat {
+    // MVC 패턴 및 프로젝트 제작 관련 멘토링
+}
--- src/Personal_Training/PT_ForExam.java
+++ src/Personal_Training/PT_ForExam.java
@@ -3,6 +3,7 @@ package Personal_Training;
 public class PT_ForExam {
 
     public static void main(String[] args) {
+        // for - 1
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
                 System.out.print("★");
@@ -12,6 +13,7 @@ public class PT_ForExam {
         System.out.println();
         System.out.println("=======================");
 
+        // for - 2
         for(int i = 1; i <= 5; i++) {
             for(int j = 1; j <= i; j++) {
                 System.out.print("★");
