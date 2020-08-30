5c0e87f1de58a3fd9d207953f0bd29f0cbe7216c
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-08-30 23:53:11 +0900

    200830 Sun 11:52 PT, 알고리즘 코드 익숙해지기.

--- src/Personal_Training/PT_200829_Sat.java
+++ src/Personal_Training/PT_200829_Sat.java
@@ -1,5 +1,10 @@
 package Personal_Training;
 
 public class PT_200829_Sat {
-    // MVC 패턴 및 프로젝트 제작 관련 멘토링
+    // MVC 패턴 및 프로젝트 제작 관련, 비대면 멘토링 진행
+    // 집에서 멘토링 수업 들음
+
+    // 두 개의 프로젝트를 하나의 git repository로
+    // 담는 방법 테스트 해보는 중..
+
 }
---
+++ src/Personal_Training/PT_AntAlgo.java
@@ -0,0 +1,41 @@
+package Personal_Training;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PT_AntAlgo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int testCnt = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < testCnt; i++) {
+            st = new StringTokenizer(br.readLine());
+            int length = Integer.parseInt(st.nextToken());
+            int antCnt = Integer.parseInt(st.nextToken());
+
+            int ants[] = new int[antCnt];
+
+            int min = Integer.MIN_VALUE;
+            int max = Integer.MAX_VALUE;
+
+            for(int j = 0; j < antCnt; j++) {
+                st = new StringTokenizer(br.readLine());
+                int ant = Integer.parseInt(st.nextToken());
+
+                int antMin = Math.min(ant, length - ant);
+                int antMax = Math.max(ant, length - ant);
+
+                min = Math.max(min, antMin);
+                max = Math.max(max, antMax);
+            }
+            System.out.println(min + " " + max);
+        }
+
+    }
+
+
+}
