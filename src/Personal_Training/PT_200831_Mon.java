f19c83ae6ba1c0b894d029636746b533e83f0f1d
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-08-31 23:50:26 +0900

    200831 Mon 11:50 PT, 디자인 패턴-23 익숙해지기.

---
+++ src/Personal_Training/PT_200831_Mon.java
@@ -0,0 +1,41 @@
+package Personal_Training;
+
+public class PT_200831_Mon {
+    // 디자인 패턴 종류 익히기
+    //Creational patterns(생성 패턴 5개) - 객체
+    //
+    //    Prototype(프로토타입 - 원형)
+    //    Singleton(싱글톤 - 1)
+    //    Builder(빌더 - 빌드오더)
+    //    Abstract factory(추상팩 - 설계)
+    //    Factory method(팩토리 - 생산)
+    //
+    //===================================================
+    //Structural patterns(구조 패턴 7개) - 프로퍼티
+    //
+    //    Adapter(어댑터 - 전원올림)
+    //    Bridge(브리지 - 다리)
+    //    Composite(합성(컴포지션, 뷰도 해당) - 켜졌다)
+    //    Decorator(데코레이터 - 장식등이 껌뻑)
+    //    Proxy(프록시 - 대리인)
+    //    Facade(파사드 - 녹슨 문이 있다)
+    //    Flyweight(플라이웨이트 - 흔들리는)
+    //
+    //===================================================
+    //Behavioral patterns(행동 패턴 13개) - 메소드
+    //
+    //    Iterator(반복자)
+    //    Command(커맨드)
+    //    Chain of responsibility(책임 연쇄)
+    //
+    //    Template method (템플릿 메서드)
+    //    Visitor (방문자)
+    //
+    //    Interpreter (해석자)
+    //    Observer (옵저버)
+    //    Strategy (전략)
+    //
+    //    Mediator (중재자)
+    //    Memento (기념품)
+    //    State (상태)
+}
